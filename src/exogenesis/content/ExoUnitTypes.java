6514a7adcdbc023700b0fa7ce0581775169c9652
SiriusA2 <NieanderSilvania@gmail.com>
2025-02-04 19:35:00 -0400

    Fortress map

--- src/exogenesis/content/ExoUnitTypes.java
+++ src/exogenesis/content/ExoUnitTypes.java
@@ -2076,7 +2076,6 @@ public class ExoUnitTypes {
                             moves.add(new PartMove(PartProgress.recoil.curve(Interp.pow2In), 10, 0, 5));
                             progress = PartProgress.charge.curve(Interp.circleIn);
                             mirror = true;
-                            outlineLayerOffset = 0.0002f;
                         }},
                         new RegionPart("-bottom"){{
                             moveX = 4;
