3d7f99e4dfa1e2df37763058a79e3753f8bfeba5
SiriusA2 <NieanderSilvania@gmail.com>
2025-07-24 13:17:22 -0400

    Vanstar unit stuff

--- src/exogenesis/content/ExoSounds.java
+++ src/exogenesis/content/ExoSounds.java
@@ -9,34 +9,54 @@ import mindustry.Vars;
 
 import java.lang.reflect.Field;
 
-public class ExoSounds{
+public class ExoSounds {
     public static Sound
-            cannon, coolplasmaboom, jumpIn,
-            energyRecharge = new Sound(), energyPew = new Sound(),
-            heavyEnergyCannon = new Sound(), energyCannon = new Sound(), cannonFire = new Sound(),
-            shotgun1 = new Sound(), energyShotgun = new Sound(), rampUpCharge = new Sound(),
-            continousWeaponFire = new Sound(), energyBolt = new Sound(), laserPew = new Sound(),
-            energyShoot1 = new Sound(), energyShoot2 = new Sound(), energyShoot3 = new Sound(),
-            heavyRound = new Sound(), rocketDeploy1 = new Sound(), rocketDeploy = new Sound(),
-            heavyShot1 = new Sound(), heavyShot = new Sound(), chargeup = new Sound(),
-            red = new Sound(),
-            matter = new Sound(),
-            thunderstorm = new Sound(),
-            jupiterImpact = new Sound(),
-            jupiterShoot = new Sound(),
-            funnylaserloop = new Sound(),
-            bigLaserShoot = new Sound(),
-            shockblast = new Sound();
-
+            coolplasmaboom = new Sound(),
+    energyRecharge = new Sound(), energyPew = new Sound(),
+    heavyEnergyCannon = new Sound(), energyCannon = new Sound(), cannonFire = new Sound(),
+    shotgun1 = new Sound(), energyShotgun = new Sound(), rampUpCharge = new Sound(),
+    continousWeaponFire = new Sound(), energyBolt = new Sound(), laserPew = new Sound(),
+    energyShoot1 = new Sound(), energyShoot2 = new Sound(), energyShoot3 = new Sound(),
+    heavyRound = new Sound(), rocketDeploy1 = new Sound(), rocketDeploy = new Sound(),
+    heavyShot1 = new Sound(), heavyShot = new Sound(), chargeup = new Sound(),
+    red = new Sound(),
+    matter = new Sound(),
+    thunderstorm = new Sound(),
+    jupiterImpact = new Sound(),
+    jupiterShoot = new Sound(),
+    funnylaserloop = new Sound(),
+    bigLaserShoot = new Sound(),
+    shockblast = new Sound();
     public static void load(){
-        Class<?> c = ExoSounds.class;
-        Seq<Field> fields = new Seq<>(c.getFields());
-        fields.select(f -> Sound.class.equals(f.getType()));
-        try{
-            for(Field f : fields)f.set(null, loadSound(f.getName()));
-        }catch(IllegalAccessException e){
-            e.printStackTrace();
-        }
+                coolplasmaboom = loadSound("coolplasmaboom");
+                energyRecharge = loadSound("energy-recharge");
+                energyPew = loadSound("energy-pew");
+                heavyEnergyCannon = loadSound("heavy-energy-cannon");
+                energyCannon = loadSound("energy-cannon");
+                cannonFire = loadSound("cannon-fire");
+                shotgun1 = loadSound("shotgun1");
+                energyShotgun = loadSound("energy-shotgun");
+                rampUpCharge = loadSound("rampUp-charge");
+                continousWeaponFire = loadSound("continousWeaponFire");
+                energyBolt = loadSound("energy-bolt");
+                laserPew = loadSound("laser-pew");
+                energyShoot1 = loadSound("energy-shoot1");
+                energyShoot2 = loadSound("energy-shoot2");
+                energyShoot3 = loadSound("energy-shoot3");
+                heavyRound = loadSound("heavy-round");
+                rocketDeploy1 = loadSound("rocket-deploy1");
+                rocketDeploy = loadSound("rocket-deploy");
+                heavyShot1 = loadSound("heavy-shot1");
+                heavyShot = loadSound("heavy-shot");
+                chargeup = loadSound("chargeup");
+                red = loadSound("red");
+                matter = loadSound("matter");
+                thunderstorm = loadSound("thunderstorm");
+                jupiterImpact = loadSound("jupiterImpact");
+                jupiterShoot = loadSound("jupiterShoot");
+                funnylaserloop = loadSound("funnylaserloop");
+                bigLaserShoot = loadSound("bigLaserShoot");
+                shockblast = loadSound("shockblast");
     }
 
     private static Sound loadSound(String soundName){
@@ -48,7 +68,11 @@ public class ExoSounds{
 
             AssetDescriptor<?> desc = Core.assets.load(path, Sound.class, new SoundLoader.SoundParameter(sound));
             desc.errored = Throwable::printStackTrace;
+
             return sound;
-        }else return new Sound();
+
+        }else{
+            return new Sound();
+        }
     }
 }
