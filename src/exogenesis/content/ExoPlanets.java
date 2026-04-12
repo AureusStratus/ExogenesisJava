813fb5421ef59d01226065a39bca5b93878a4105
SiriusA2 <NieanderSilvania@gmail.com>
2026-04-11 21:48:39 -0400

    Serpulo/Erekir updates

--- src/exogenesis/content/ExoPlanets.java
+++ src/exogenesis/content/ExoPlanets.java
@@ -232,6 +232,7 @@ public class ExoPlanets{
                 );
             }};
         */
+            meshLoader = () -> new HexMesh(this, 6);
             generator = new VanstarPlanetGenerator();
             Prov<GenericMesh> atmosphereMeshLoader = () -> new MultiMesh(
                     new NoiseMesh(vanstar, 0, 6, Color.valueOf("56c7e1"), 1, 1, 1, 4, 0.025f) {{
@@ -263,7 +264,7 @@ public class ExoPlanets{
             );
             meshLoader = () -> new MultiMesh(
                     new AtmosphereMesh(vanstar, atmosphereMeshLoader.get()),
-                    new NoiseMesh(vanstar, 0, 6, Color.valueOf("56c7e1"), 1, 1, 1, 4, 0.025f),
+                    new NoiseMesh(vanstar, 0, 6, Color.valueOf("56c7e1"), 1, 1, 1, 4, 0f),
                     new HeightMesh(vanstar, 6, 0.85f, position -> {
                         int seed = 3;
                         double octaves = 7, persistence = 0.7, scale = 0.25;
