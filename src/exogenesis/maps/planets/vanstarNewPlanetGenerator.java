f827e956094e664b67ba32bcd3466911a53b432a
SiriusA2 <NieanderSilvania@gmail.com>
2026-04-27 08:38:46 -0400

    Empyrean Tierless units adjustments 1

--- src/exogenesis/maps/planets/vanstarNewPlanetGenerator.java
+++ src/exogenesis/maps/planets/vanstarNewPlanetGenerator.java
@@ -87,7 +87,7 @@ public class vanstarNewPlanetGenerator extends PlanetGenerator{
 
         float height = rawHeight(position);
         float depth = Simplex.noise3d(baseSeed, 4, 0.7f, 0.5f, position.x, position.y, position.z);
-        float mountains = Ridged.noise3d(baseSeed, pos.x, pos.y, pos.z, 5, 0.5f);
+        float mountains = Ridged.noise3d(baseSeed, pos.x, pos.y, -5, 5, 0.5f);
         float IceTex = Ridged.noise3d(baseSeed+3, pos.x, pos.y, pos.z, 5, 0.65f);
         float IceWaterTex = Ridged.noise3d(baseSeed+3, pos.x, pos.y, pos.z, 4, 0.6f);
         /** 0 at poles and 1 at equator */
