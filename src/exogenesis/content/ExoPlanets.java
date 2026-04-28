61b21d647cdbac03eb8f940015ad104b959352e9
SiriusA2 <NieanderSilvania@gmail.com>
2026-04-28 03:49:27 -0400

    Empyrean Tierless units adjustments 1

--- src/exogenesis/content/ExoPlanets.java
+++ src/exogenesis/content/ExoPlanets.java
@@ -357,9 +357,9 @@ public class ExoPlanets{
 
             meshLoader = () -> new MultiMesh(
                     new HexMesh(this, 6),
-                    new HexSkyMesh(this, 1, 0.65f, 0.17f, 4, new Color().set(Color.white).mul(0.9f).a(0.35f), 7, 0.45f, 0.6f, 0.20f),
-                    new HexSkyMesh(this, 2, 0.85f, 0.16f, 5, new Color().set(Color.white).mul(0.9f).a(0.65f), 6, 0.45f, 0.7f, 0.30f),
-                    new HexSkyMesh(this, 3, 1.15f, 0.15f, 6, new Color().set(Color.white).mul(0.9f).a(0.95f), 6, 0.65f, 0.35f, 0.6f)
+                    new HexSkyMesh(this, 1, 0.65f, 0.17f, 4, new Color().set(Color.white).a(0.35f), 7, 0.45f, 0.6f, 0.20f),
+                    new HexSkyMesh(this, 2, 0.85f, 0.16f, 5, new Color().set(Color.white).a(0.65f), 6, 0.45f, 0.7f, 0.30f),
+                    new HexSkyMesh(this, 3, 1.15f, 0.15f, 6, new Color().set(Color.white).a(0.95f), 6, 0.65f, 0.35f, 0.6f)
                     /*
                     new CircleMesh(atlas.find("exogenesis-ring3"), this, 80, 3.5f, 2.6f, ringPos),
 
