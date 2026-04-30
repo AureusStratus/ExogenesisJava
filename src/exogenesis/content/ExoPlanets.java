189400af9407348d0c651b58bce75e1d4c26617b
SiriusA2 <NieanderSilvania@gmail.com>
2026-04-30 19:50:20 -0400

    Empyrean Tierless units adjustments 1

--- src/exogenesis/content/ExoPlanets.java
+++ src/exogenesis/content/ExoPlanets.java
@@ -643,7 +643,7 @@ public class ExoPlanets{
             */
             generator = new AxinPlanetGenerator();
             meshLoader = () -> new MultiMesh(
-                    new HexMesh(this, 5),
+                    new HexMesh(this, 6),
                     new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Color.blue).mul(0.9f).a(0.55f), 2, 0.45f, 0.9f, 0.38f),
                     new HexSkyMesh(this, 1, 0.6f, 0.16f, 6, Color.white.cpy().lerp(Color.blue, 0.55f).a(0.25f), 2, 0.45f, 1f, 0.61f)
             );
